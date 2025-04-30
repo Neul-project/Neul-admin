b5f16486620432f5407e4859a34f074cccc9c170
ihoek <ihoek679@naver.com>
2025-05-01 00:23:47 +0900

    feat : 활동기록 등록 페이지 제작

--- neul-admin/src/features/ActivitySubmit/index.tsx
+++ neul-admin/src/features/ActivitySubmit/index.tsx
@@ -1,17 +1,33 @@
-import { ActivityStyled } from "./styled";
+import { ActivityStyled, ActivityTheme } from "./styled";
 import { useFormik } from "formik";
 
 //antd
-import { Input, Upload, message, Button } from "antd";
+import {
+  Input,
+  Upload,
+  message,
+  Button,
+  Select,
+  Radio,
+  ConfigProvider,
+} from "antd";
 import { UploadOutlined } from "@ant-design/icons";
-import type { UploadProps } from "antd";
+import type { UploadProps, RadioChangeEvent } from "antd";
+const { TextArea } = Input;
 
 //Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/pagination";
+
+import { useState } from "react";
+import { Pagination, A11y } from "swiper/modules";
 
 //활동 기록 등록 컴포넌트 - formik 작성
 const ActivitySubmit = () => {
+  //useState
+  const [imgarr, setImgarr] = useState([""]);
+
   //파일 업로드
   const props: UploadProps = {
     onChange(info) {
@@ -29,6 +45,18 @@ const ActivitySubmit = () => {
   //swiper array -> 파일 업로드 시 파일 이미지 리스트 나올것
   const arr = [1, 2, 3, 4, 5];
 
+  //활동 종류 select
+  const select_option = [
+    { value: "walk", label: "산책" },
+    { value: "play", label: "놀이" },
+    { value: "exercise", label: "운동" },
+  ];
+
+  //라디오버튼
+  const onChange = (e: RadioChangeEvent) => {
+    console.log(`radio checked:${e.target.value}`);
+  };
+
   //formik
   const activityformik = useFormik({
     initialValues: {},
@@ -43,22 +71,92 @@ const ActivitySubmit = () => {
         className="activitySubmit_form"
         onSubmit={activityformik.handleSubmit}
       >
-        <Input placeholder="제목을 입력하시오" />
-        <Upload {...props}>
-          <Button icon={<UploadOutlined />}>Click to Upload</Button>
-        </Upload>
-        <div className="activitySubmit_swiper_div">
-          <Swiper
-            className="activitySubmit_swiper"
-            spaceBetween={50}
-            slidesPerView={1}
-            onSlideChange={() => console.log("slide change")}
-            //onSwiper={(swiper) => console.log(swiper)}
-          >
-            {arr.map((element) => (
-              <SwiperSlide>{element}</SwiperSlide>
-            ))}
-          </Swiper>
+        {/* 제목 */}
+        <div className="activitySubmit_title">
+          <div className="activitySubmit_text">제목</div>
+          <ConfigProvider theme={ActivityTheme}>
+            <Input
+              className="activitySubmit_title_input"
+              placeholder="제목을 입력하시오"
+            />
+          </ConfigProvider>
+        </div>
+
+        {/* swiper */}
+        <div className="activitySubmit_image">
+          <Upload {...props}>
+            <Button icon={<UploadOutlined />}>이미지 업로드</Button>
+          </Upload>
+          <div className="activitySubmit_swiper_div">
+            <Swiper
+              modules={[Pagination]}
+              className="activitySubmit_swiper"
+              spaceBetween={50}
+              slidesPerView={1}
+              pagination={{ clickable: true }}
+              //onSlideChange={() => console.log("slide change")}
+              //onSwiper={(swiper) => console.log(swiper)}
+            >
+              {arr.map((element: any, index: number) => (
+                <SwiperSlide key={index}>{element}</SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+
+        {/* 활동종류 & 재활 치료 */}
+        <div className="activitySubmit_type">
+          {/* 활동종류 */}
+          <div>
+            <div className="activitySubmit_text">활동 종류</div>
+            <ConfigProvider theme={ActivityTheme}>
+              <Select
+                className="activitySubmit_select"
+                defaultValue="walk"
+                //onChange={handleChange}
+                options={select_option}
+              />
+            </ConfigProvider>
+          </div>
+
+          {/* 재활 치료 */}
+          <div>
+            <div className="activitySubmit_text">재활 치료</div>
+            <div>
+              <Radio.Group onChange={onChange}>
+                <Radio.Button
+                  className="activitySubmit_radio"
+                  value="Participation"
+                >
+                  참여
+                </Radio.Button>
+                <Radio.Button
+                  className="activitySubmit_radio"
+                  value="nonParticipation"
+                >
+                  미참여
+                </Radio.Button>
+                <Radio.Button
+                  className="activitySubmit_radio"
+                  value="nonTarget"
+                >
+                  비대상
+                </Radio.Button>
+              </Radio.Group>
+            </div>
+          </div>
+        </div>
+
+        {/* 특이 사항 */}
+        <div>
+          <div className="activitySubmit_text">특이 사항</div>
+          <ConfigProvider theme={ActivityTheme}>
+            <TextArea rows={7} />
+          </ConfigProvider>
+        </div>
+
+        <div className="activitySubmit_record_div">
+          <Button className="activitySubmit_record">기록하기</Button>
         </div>
       </form>
     </ActivityStyled>
--- neul-admin/src/features/ActivitySubmit/styled.ts
+++ neul-admin/src/features/ActivitySubmit/styled.ts
@@ -5,18 +5,84 @@ export const ActivityStyled = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
-    align-items: center;
-  }
-  .activitySubmit_swiper_div {
-    width: 90%;
-    height: 300px;
-    border-radius: 15px;
-    padding: 15px;
-    border: 1px solid black;
+    //align-items: center;
+    gap: 25px;
 
-    .activitySubmit_swiper {
+    .activitySubmit_text {
+      font-size: 14px;
+      margin-bottom: 5px;
+    }
+
+    .activitySubmit_title {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .activitySubmit_title_input {
+        width: 95%;
+      }
+    }
+
+    .activitySubmit_image {
+      .activitySubmit_swiper_div {
+        width: 100%;
+        height: 300px;
+        border-radius: 15px;
+        padding: 15px;
+        border: 1px solid black;
+        margin-top: 15px;
+
+        .activitySubmit_swiper {
+          width: 100%;
+          height: 100%;
+          cursor: pointer;
+        }
+      }
+    }
+
+    .activitySubmit_type {
+      display: flex;
+      gap: 20px;
       width: 100%;
-      height: 100%;
+
+      .activitySubmit_select {
+        width: 250px;
+      }
+      .activitySubmit_radio {
+        width: 120px;
+        text-align: center;
+      }
+    }
+
+    .activitySubmit_record_div {
+      display: flex;
+      justify-content: center;
+      width: 100%;
+      margin-top: 50px;
+
+      .activitySubmit_record {
+        width: 120px;
+      }
     }
   }
 `;
+
+export const ActivityTheme = {
+  token: {
+    // global token
+  },
+  components: {
+    // component token
+    Input: {
+      activeBorderColor: "none",
+      activeShadow: "none",
+      hoverBorderColor: "none",
+    },
+    Select: {
+      activeBorderColor: "none",
+      activeShadow: "none",
+      hoverBorderColor: "none",
+      activeOutlineColor: "none",
+    },
+  },
+};
