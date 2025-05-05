879a4342c737e3bb2ba17cb6e5f21134c99dab45
ihoek <ihoek679@naver.com>
2025-05-06 01:09:40 +0900

    feat : 배너 광고 등록 페이지 제작

---
+++ neul-admin/src/features/Banner/styled.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const BannerStyled = styled.div`
+  &.Banner_main_wrap {
+    .Banner_title {
+      font-size: 24px;
+      margin-bottom: 15px;
+    }
+
+    .Banner_imgs {
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      gap: 20px;
+
+      .Banner_left_img,
+      .Banner_right_img {
+        width: 50%;
+        height: 400px;
+        border: 1px solid #ccc;
+        border-radius: 15px;
+        overflow: hidden;
+
+        .Banner_imgstyle {
+          width: 100%;
+          height: 100%;
+          object-fit: contain;
+        }
+        .Banner_preview_text {
+          display: flex;
+          width: 100%;
+          height: 100%;
+          justify-content: center;
+          align-items: center;
+        }
+      }
+    }
+    .Banner_btns {
+      display: flex;
+      justify-content: space-between;
+      padding: 0px 120px;
+      padding-top: 20px;
+    }
+    .Banner_save {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      margin-top: 50px;
+    }
+  }
+`;
