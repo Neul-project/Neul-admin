f4bb01fe1b5fe849546a6f1c319d53ec1ef232e1
werther <65219453+werther901@users.noreply.github.com>
2025-05-16 20:01:14 +0900

    Feat: 환불리스트 요청

---
+++ neul-admin/src/features/Refund/styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const RefundStyled = styled.div`
+  .my-divider {
+    width: 100%;
+    height: 10px;
+    background-color: #e0e0e0;
+    margin: 16px 0;
+  }
+`;
+
+export const Divider = styled.div`
+  height: 1px;
+  background-color: #ddd;
+  margin: 12px 0;
+`;
