498ebc4580aff83e402480049b98d617050ca242
ihoek <ihoek679@naver.com>
2025-05-18 23:36:58 +0900

    fix : 모달 css 수정

--- neul-admin/src/features/ActivityList/styled.ts
+++ neul-admin/src/features/ActivityList/styled.ts
@@ -47,9 +47,9 @@ export const ActivityListStyled = styled.div`
   }
 `;
 
-export const StyledModal = styled(Modal)`
+export const ㄹ = styled(Modal)`
   .ant-modal-content {
-    padding: 20px 10px 20px 20px;
+    padding: 0px 10px 20px 20px;
 
     .ActivityList_Modal {
       max-height: 70vh;
