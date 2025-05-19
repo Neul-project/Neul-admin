45bfaba9c763ba2f49a206f2d4415d1b7d22a9d9
ihoek <ihoek679@naver.com>
2025-05-19 19:38:57 +0900

    fix : 용어 변경

--- neul-admin/src/features/Feedback/index.tsx
+++ neul-admin/src/features/Feedback/index.tsx
@@ -35,10 +35,11 @@ interface AdminType {
 //피드백 리스트 컴포넌트
 const Feedback = () => {
   //변수 선언
-  const [list, setList] = useState<DataType[]>();
-  const [adminlist, setAdminlist] = useState<AdminType[]>();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedRecord, setSelectedRecord] = useState<DataType | null>(null);
+  const [list, setList] = useState<DataType[]>(); //피드백 배열 관리
+  const [adminlist, setAdminlist] = useState<AdminType[]>(); //select 관리
+  const [isModalOpen, setIsModalOpen] = useState(false); //상세 모달 여부확인
+  const [selectedRecord, setSelectedRecord] = useState<DataType | null>(null); //모달로 넘길 내용
+  const [searchValue, setSearchValue] = useState(""); //검색 내용
 
   //테이블 열
   const columns: TableProps<DataType>["columns"] = [
@@ -67,7 +68,7 @@ const Feedback = () => {
 
   //활동기록 검색
   const onSearch: SearchProps["onSearch"] = (value, _e, info) => {
-    console.log("value", value);
+    //console.log("value", value);
 
     //피드백 : 활동기록 제목(title) 검색에 따른 행(피드백) 반환 요청
     axiosInstance
@@ -75,19 +76,15 @@ const Feedback = () => {
       .then((res) => {
         const data = res.data;
         setList(data);
+        setSearchValue("");
       });
   };
 
-  useEffect(() => {
-    axiosInstance.get(`/user/adminlist`).then((res) => {
-      const data: AdminType[] = res.data;
-      setAdminlist([{ value: 0, label: "전체" }, ...data]);
-    });
-
-    //피드백 내용 전체 불러오기
+  //피드백 내용 전체 불러오기
+  const feedbackviews = () => {
     axiosInstance.get(`/activity/feedback/views`).then((res) => {
       const data = res.data;
-      console.log("data", data);
+      //console.log("data", data);
       const mappedList: DataType[] = data.map((item: any, index: number) => ({
         key: item.id,
         number: index + 1,
@@ -99,14 +96,13 @@ const Feedback = () => {
       }));
       setList(mappedList);
     });
-  }, []);
+  };
 
-  const handleChange = (option: { value: number; label: string }) => {
-    //console.log(`selected ${option.value}`);
-    const admin = option.value;
-    if (admin === 0) {
-      //전체 feedback내용 보여지기
-      axiosInstance.get("/activity/feedback/views").then((res) => {
+  //admin id에 해당하는 피드백 내용 가져오기
+  const feedbackview = (admin: any) => {
+    axiosInstance
+      .get("/activity/feedback/view", { params: { adminId: admin } })
+      .then((res) => {
         const data = res.data;
         const mappedList: DataType[] = data.map((item: any, index: number) => ({
           key: item.id,
@@ -120,28 +116,25 @@ const Feedback = () => {
 
         setList(mappedList);
       });
+  };
+
+  useEffect(() => {
+    axiosInstance.get(`/user/adminlist`).then((res) => {
+      const data: AdminType[] = res.data;
+      setAdminlist([{ value: 0, label: "전체" }, ...data]);
+    });
+    feedbackviews();
+  }, []);
+
+  const handleChange = (option: { value: number; label: string }) => {
+    //console.log(`selected ${option.value}`);
+    const admin = option.value;
+    if (admin === 0) {
+      feedbackviews();
     } else {
       //도우미 id에 해당하는 feedback내용 보여지기
       //console.log("Ad", admin);
-      axiosInstance
-        .get("/activity/feedback/view", { params: { adminId: admin } })
-        .then((res) => {
-          //console.log("activity feedback view res", res.data);
-          const data = res.data;
-          const mappedList: DataType[] = data.map(
-            (item: any, index: number) => ({
-              key: item.id,
-              number: index + 1,
-              activity: item.activity.title,
-              content: item.message,
-              date: formatDate(item.recorded_at),
-              admin: item.activity.id,
-              origin: item,
-            })
-          );
-
-          setList(mappedList);
-        });
+      feedbackview(admin);
     }
   };
   return (
@@ -165,6 +158,8 @@ const Feedback = () => {
             <Search
               placeholder="활동기록 명을 입력하시오."
               onSearch={onSearch}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
               className="Feedback_search"
             />
           </ConfigProvider>
