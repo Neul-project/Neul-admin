5db27de444712d24912d89bc658b82d7b07474e6
Taetae1123 <xodus2079@naver.com>
2025-05-28 22:15:39 +0900

    Fix: 헬스체크 확인

--- neul-admin/src/pages/_app.tsx
+++ neul-admin/src/pages/_app.tsx
@@ -26,7 +26,8 @@ export default function App({ Component, pageProps }: AppProps) {
   // 서버 체크
   const checkServer = async () => {
     try {
-      await axios.get("/auth/health"); // 백엔드의 헬스 체크
+      const res = await axios.get("/auth/health"); // 백엔드의 헬스 체크
+      console.log(res.data, "1!!!!!!!!!!!!!!!!!");
       setIsServerDown(false);
     } catch (error) {
       setIsServerDown(true);
